5fba5cfb834045da1cae711155e1f48b7748f2f0
elzuoc <elzuoc@gmail.com>
2022-10-14 11:00:54 +0800

    feat:Create React Project, tailwindcss and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'max-len': ['warn', { code: 80 }],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+  },
+};
