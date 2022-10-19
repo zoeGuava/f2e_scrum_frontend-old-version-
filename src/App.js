f55475a3326bc8ed3e874e670516e074b73d7339
elzuoc <elzuoc@gmail.com>
2022-10-19 09:24:50 +0800

    fix:use lowercase to rename all file in views/

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
 import NavBar from './components/NavBar';
+import ExampleLearn from './views/learn';
+import ExampleChallenge from './views/challenge';
+import IndexPage from './views';
 import './App.css';
 
 function App() {
@@ -7,6 +15,14 @@ function App() {
     <div className="App">
       <NavBar />
 
+      <hr />
+      <Router>
+        <Routes>
+          <Route path="/" element={<IndexPage />} />
+          <Route path="/learn" element={<ExampleLearn />} />
+          <Route path="/challenge" element={<ExampleChallenge />} />
+        </Routes>
+      </Router>
     </div>
 
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import {
   BrowserRouter as Router,
-  Routes,
-  Route,
   Link,
 } from 'react-router-dom';
-import ExampleLearn from '../views/examplelearn';
-import ExampleChallenge from '../views/examplechallenge';
-import IndexPage from '../views';
 
 function NavBar() {
   return (
@@ -18,20 +13,12 @@ function NavBar() {
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/ExampleLearn">Learn Example</Link>
+            <Link to="/learn">Learn Example</Link>
           </li>
           <li>
-            <Link to="/ExampleChallenge">Challenge Example</Link>
+            <Link to="/challenge">Challenge Example</Link>
           </li>
         </ul>
-
-        <hr />
-
-        <Routes>
-          <Route path="/" element={<IndexPage />} />
-          <Route path="/ExampleLearn" element={<ExampleLearn />} />
-          <Route path="/ExampleChallenge" element={<ExampleChallenge />} />
-        </Routes>
 
       </div>
     </Router>
